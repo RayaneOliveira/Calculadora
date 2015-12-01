4e84bf829fda8e34a4ddb8536736cd8bf5048b22
IFPB <IFPB@192.168.1.33>
2015-12-01 12:03:54 -0300

    jnsfnjfs

---
+++ src/CalculatorServer.java
@@ -0,0 +1,18 @@
+import java.rmi.Naming;
+
+public class CalculatorServer {
+	
+	public CalculatorServer() {
+		
+		try {
+			RayaneCalculator c = new CalculatorImpl();
+			Naming.rebind("rmi://jaca.ime.usp.br:1099/CalculatorService", c);
+		} catch (Exception e) {
+			System.out.println("Trouble: " + e);
+		}
+	}
+
+	public static void main(String args[]) {
+		new CalculatorServer();
+	}
+}
